924ae9460d6384cdd60d4e38553d232647d9e397
Jerome Leclanche <jerome@leclan.ch>
2022-11-05 00:58:30 +0100

    Use a config file

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,50 @@
 #!/usr/bin/env node
 
 import { render } from "@faire/mjml-react";
+import Conf from "conf";
 import { readdir, readFile } from "fs/promises";
 import i18next from "i18next";
-import path from "path";
+import { join, parse, resolve } from "path";
 import FilesystemBackend from "./backends/filesystem";
 import SendgridBackend from "./backends/sendgrid";
 
-const EMAILS_DIR = "build/emails/";
-const OUT_DIR = "out/";
-const DEFAULT_LOCALE = "en";
-const I18N_RESOURCES = path.resolve("src/i18n.json");
-const SUPPORTED_LANGUAGES = ["en", "es"];
-const VALIDATION_LEVEL = "strict";
+type ValidationLevel = "strict" | "soft" | "skip";
 
 export default (async () => {
-	const resources = JSON.parse((await readFile(I18N_RESOURCES)).toString());
+	const config = new Conf({
+		cwd: ".",
+		projectName: "mjmgr",
+		configName: "mjmgr",
+	});
+
+	const supportedLanguages = config.get("supported_languages", ["en"]) as string[];
+	const validationLevel = config.get("validation_level", "strict") as ValidationLevel;
+	const outDir = config.get("output_dir", "./out/") as string;
+	const i18nPath = config.get("i18n_path", "./src/i18n.json") as string;
+	const emailsDir = config.get("emails_dir", "./src/emails/") as string;
+
+	const resources = JSON.parse((await readFile(resolve(i18nPath))).toString());
 
 	i18next.init({
 		resources: resources,
 		defaultNS: "common",
-		lng: DEFAULT_LOCALE,
 	});
 
-	const files = await readdir(EMAILS_DIR);
-	const backends = [new FilesystemBackend(OUT_DIR), new SendgridBackend()];
+	const files = await readdir(emailsDir);
+	const backends = [new FilesystemBackend(outDir), new SendgridBackend()];
 
-	for (const locale of SUPPORTED_LANGUAGES) {
+	for (const locale of supportedLanguages) {
 		const t = await i18next.changeLanguage(locale);
 
 		for (const file of files) {
-			const modulePath = path.resolve(path.join(EMAILS_DIR, file));
+			const modulePath = resolve(join(emailsDir, file));
 			const component = (await import(modulePath)).default;
 
 			const { html } = render(component({ locale, t }), {
-				validationLevel: VALIDATION_LEVEL,
+				validationLevel,
 			});
 
-			const templateBaseName = path.parse(file).name;
+			const templateBaseName = parse(file).name;
 			const templateName = `${templateBaseName}_${locale}`;
 
 			backends.forEach(async (backend) => {
